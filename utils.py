527b9039ad1417b3f122a07893a247256f7ed0fe
Nesya <angerasimov04@outlook.com>
2021-04-19 18:05:48 +0300

    mistake dp.

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 def to_byte_array(s, num_symbols, num_colors, repetition):
     if len(s) != num_symbols:
-        return 'Неправильное кол-во символов.'
+        return f'Неправильное кол-во символов.\n' \
+               f'Их должно быть ровно {num_symbols}.'
 
     array = bytearray()
     for i in s:
@@ -9,9 +10,11 @@ def to_byte_array(s, num_symbols, num_colors, repetition):
             if 1 <= t <= num_colors:
                 array.append(t - 1)
             else:
-                return 'Одна из цифр неправильная.'
+                return f'Можно вводить только цифры от 1 до {num_colors}.\n' \
+                       f'У вас присутствует цифра {t}.'
         else:
-            return 'Одна из цифр неправильная.'
+            return f'Можно вводить только цифры от 1 до {num_colors}.\n' \
+                   f'У вас присутствует символ "{i}".'
 
     if not repetition:
         if len(array) != len(set(array)):
